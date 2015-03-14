0d48a0f34524906b05111e212756cc57f81f79d5
Harish Anchu <harishanchu@gmail.com>
2015-03-15 00:26:04 +0530

    Module main script file added

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * index.js
+ *
+ * @author: Harish Anchu <harishanchu@gmail.com>
+ * @copyright 2015, QuorraJS. All rights reserved.
+ * @license Licensed under MIT
+ */
+
+var Quorra = require('./lib/foundation/Application');
+
+// Instantiate and expose a Quorra singleton
+module.exports = new Quorra();
