0fc3a43f3c3dafd3f4687a4ffe22b2a328875903
Harish Anchu <harishanchu@gmail.com>
2016-03-31 01:27:54 +0530

    Add tests for StackedHttpKernel

---
+++ test/unit/stack/stackedHttpKernel.spec.js
@@ -0,0 +1,31 @@
+var StackedHttpKernel = require('../../../lib/stack/StackedHttpKernel');
+
+describe('StackedHttpKernel', function () {
+   describe('#constructor', function () {
+      it('should return an instance of StackedHttpKernel when initialized', function (done) {
+          var stack = new StackedHttpKernel();
+
+          stack.should.be.an.instanceOf(StackedHttpKernel);
+
+          done();
+      });
+   });
+
+    describe('#handle', function () {
+        it('should call handle method of app instance with passed request and response arguments', function (done) {
+            var request = {};
+            var response = {};
+            var app = {
+                handle: function (req, res) {
+                    req.should.be.equal(request);
+                    res.should.be.equal(response);
+
+                    done();
+                }
+            };
+            var stack = new StackedHttpKernel(app);
+
+            app.handle(request, response);
+        });
+    })
+});
